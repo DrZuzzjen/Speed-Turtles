689e96917c196bb713cbfbefb1a91561563d8f2c
DrZuzzjen <63299382+DrZuzzjen@users.noreply.github.com>
2020-05-12 16:20:09 +0200

    Nuevo menmain funcionando

--- main.py
+++ main.py
@@ -2,12 +2,34 @@ import turtle
 
 class Circuito():
     corredores: []
+    __posStartY = (-30, -10, 10, 30)
+    __colorT = ('red', 'blue', 'green', 'orange')
+    
     
     def __init__(self, ancho, alto):
         self.__screen = turtle.Screen()  
         self.__screen.setup(ancho, alto)
-        self.__screen.bgcolor('lightgray')
+        self.__screen.bgcolor('black')
+        self.__startLine = -ancho/2 +20
+        self.__finishLine = ancho/2 -20
         
+        for i in range(4):
+            tortuga = turtle.Turtle()
+            tortuga.color(self.__colorT[i])
+            tortuga.shape("turtle")
+            tortuga.penup()
+            tortuga.setpos(self.__startLine, self.__posStartY[i])
+                
+            self.corredores.append(tortuga)
+            
+            
+   
+   
+   
+   
+   
+   
+   
 if __name__ == "__main__":
     circuito = Circuito(640, 480)
         
---
+++ main2.py
@@ -0,0 +1,28 @@
+import turtle
+
+class Circuito():
+    corredores = []
+    __posStartY =(-30, -10, 10, 30)
+    __colorTurtle = ('red', 'blue', 'green', 'orange')
+    
+    def __init__(self, ancho, alto):
+        self.__screen = turtle.Screen()  
+        self.__screen.setup(ancho, alto)
+        self.__screen.bgcolor('black')
+        self.__startLine = -ancho/2 +20
+        self.__finishLine = ancho/2 -20
+        
+        self.__CreadorTortugas()
+        
+    def __CreadorTortugas(self):
+        for i in range(4):
+            new_turtle = turtle.Turtle()
+            new_turtle.color(self.__colorTurtle[i])
+            new_turtle.shape('turtle')
+            new_turtle.penup()
+            new_turtle.setpos(self.__startLine, self.__posStartY[i])
+            
+            self.corredores.append(new_turtle)
+            
+if __name__ == '__main__':
+    circuito = Circuito(640,480)
